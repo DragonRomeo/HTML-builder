ea67b05b895ec73ed2ea215bed97ea061db3a82f
Ilya Dragonromeo <you@example.com>
2023-05-08 15:33:50 +0300

    feat: complete second task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const path = require('node:path');
-// const path = './text.txt'
 
-const filePatch = path.join('./01-read-file', 'text.txt') ;
+const filePatch = path.join(__dirname, 'text.txt') ;
 
 console.log(filePatch);
 const rs = fs.createReadStream(filePatch, 'utf8');
@@ -10,11 +9,3 @@ const rs = fs.createReadStream(filePatch, 'utf8');
 rs.on('data', function(chunk){
     console.log(chunk);
 })
-
-
-
-// fs.readFile('text.txt', 'utf8', function(error, textFile){
-//     if(error) throw error;
-//     console.log(textFile);
-// });
-// reading();
---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("node:path");
+
+const readline = require("node:readline");
+const { stdin: input, stdout: output } = require("node:process");
+
+fs.writeFile(path.join(__dirname, "mynotes.txt"), "Your text: \n", (err) => {
+  if (err) throw err;
+  console.log("Файл был создан");
+  console.log("Введите ваш текст:");
+});
+
+const rl = readline.createInterface({ input, output });
+
+rl.on("line", (input) => {
+  if(input === 'exit'){
+    rl.close();
+    console.log('Спасибо за ваши тёплые фразы');
+  } else {
+    console.log(`Received: ${input}`);
+    fs.appendFile(path.join(__dirname, "mynotes.txt"), `${input} \n`, (err) => {
+      if (err) throw err;
+      console.log("Файл был изменен");
+    });
+  }
+  
+});
+
+
